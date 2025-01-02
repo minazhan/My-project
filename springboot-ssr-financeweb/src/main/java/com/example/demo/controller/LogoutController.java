d8896ee1274984a6757591a5cce702057e0a6924
minazhan <monmonzhen@gmail.com>
2025-01-02 09:02:33 +0800

    20250102

--- springboot-ssr-financeweb/src/main/java/com/example/demo/controller/LogoutController.java
+++ springboot-ssr-financeweb/src/main/java/com/example/demo/controller/LogoutController.java
@@ -13,8 +13,8 @@ public class LogoutController {
 	//明確指定這個方法處理 HTTP GET 請求
 	@GetMapping
 	public String logout(HttpSession session) {
-        session.invalidate(); // 清除所有 Session 數據
-        return "redirect:/"; // 跳轉回登入頁面
+        session.invalidate(); //清除所有 Session 數據
+        return "redirect:/"; //跳轉回登入頁面
     }
 
 }
