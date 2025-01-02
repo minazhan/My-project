371d9910acc6d5cfe06a9c5993fe7fd9a548a965
minazhan <monmonzhen@gmail.com>
2025-01-02 09:11:54 +0800

    20250102

--- springboot-ssr-financeweb/src/main/java/com/example/demo/response/ApiResponse.java
+++ springboot-ssr-financeweb/src/main/java/com/example/demo/response/ApiResponse.java
@@ -4,22 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// 建立 Server 與 Client 在傳遞資料上的統一結構與標準(含錯誤)
+//建立 Server 與 Client 在傳遞資料上的統一結構與標準(含錯誤)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
-	private int status;     // 狀態 例如: 200, 400
-	private String message; // 訊息 例如: 查詢成功, 新增成功, 請求錯誤
-	private T data; 	    // payload 實際資料 //data返回的數據，例如查詢結果，當請求成功時會包含此數據
+	private int status;     //狀態 例如: 200, 400
+	private String message; //訊息 例如: 查詢成功, 新增成功, 請求錯誤
+	private T data; 	//payload 實際資料 //data返回的數據，例如查詢結果，當請求成功時會包含此數據
 	//泛型類（<T>），可以接受任何類型作為 data 類型
 	
-	// 成功回應
+	//成功回應
 	public static <T> ApiResponse<T> success(String message, T data) {
 		return new ApiResponse<T>(200, message, data);
 	}
 	
-	// 失敗回應
+	//失敗回應
 	public static <T> ApiResponse<T> error(int status, String message) {
 		return new ApiResponse<T>(status, message, null);
 	}
