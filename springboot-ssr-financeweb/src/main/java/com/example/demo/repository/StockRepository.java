9043cf28a2cae4fb03f3f804b8733cc1534071dc
minazhan <monmonzhen@gmail.com>
2025-01-02 09:10:36 +0800

    20250102

--- springboot-ssr-financeweb/src/main/java/com/example/demo/repository/StockRepository.java
+++ springboot-ssr-financeweb/src/main/java/com/example/demo/repository/StockRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<StockEntity, Long> {
-    Optional<StockEntity> findByStockSymbol(String stockSymbol); // 根據代碼查詢股票
+    Optional<StockEntity> findByStockSymbol(String stockSymbol); //根據代碼查詢股票
     
     //用於推薦列表
-    List<StockEntity> findTop5ByRiskLevel(String riskLevel);// 查詢符合條件的前五筆產品
+    List<StockEntity> findTop5ByRiskLevel(String riskLevel);//查詢符合條件的前五筆產品
     
     //TRUNCATE完成重置自增列的工作
     @Modifying
